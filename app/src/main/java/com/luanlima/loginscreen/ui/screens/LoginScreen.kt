6786bdb038afb1c28023ccfd77e09ea2bac5d859
luanf38 <luanfelipe38@hotmail.com>
2025-06-19 14:04:01 -0300

    first commit

---
+++ app/src/main/java/com/luanlima/loginscreen/ui/screens/LoginScreen.kt
@@ -0,0 +1,7 @@
+package com.luanlima.loginscreen.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {}
