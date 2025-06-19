cc54e0b254782ed18efad47e71ed1326710aa94c
luanf38 <luanfelipe38@hotmail.com>
2025-06-19 14:12:13 -0300

    Created appbar

--- app/src/main/java/com/luanlima/loginscreen/MainActivity.kt
+++ app/src/main/java/com/luanlima/loginscreen/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.luanlima.loginscreen.ui.screens.LoginScreen
 import com.luanlima.loginscreen.ui.theme.LoginScreenTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +28,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginApp(modifier: Modifier = Modifier) {
     LoginScreenTheme {
         Scaffold(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(title = { Text("Login") })
+            }
         ) {
             LoginScreen(modifier = Modifier.padding(it))
         }
