be3c5d210f7364556a17214dcf7cc76d8e97fce7
luanf38 <luanfelipe38@hotmail.com>
2025-06-19 14:36:47 -0300

    screens enum

--- app/src/main/java/com/luanlima/loginscreen/ui/helpers/Screens.kt
+++ app/src/main/java/com/luanlima/loginscreen/ui/helpers/Screens.kt
@@ -1,10 +1,9 @@
 package com.luanlima.loginscreen.ui.helpers
 
 import androidx.annotation.StringRes
-import androidx.compose.ui.res.stringResource
 import com.luanlima.loginscreen.R
 
-enum class Screens(@StringRes title: Int) {
+enum class Screens(@StringRes val title: Int) {
     Login(title = R.string.login),
     Home(title = R.string.home)
 }
